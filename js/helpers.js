a5db1f4d1b4da145379818cb8237457c8c6e1a67
Kevin Phan <kevin.phan10@hotmail.com>
2026-02-06 12:25:16 -0500

    fix: fixed browser links for each subring

--- js/helpers.js
+++ js/helpers.js
@@ -6,6 +6,36 @@ const formatUrl = (url) => {
       .replace(/^www\./, "");
 };
 
+const RING_PROGRAMS = new Set(["cs", "eng", "bcom", "other"]);
+
+const getProgramFromUrl = () => {
+    const pathSegment = window.location.pathname
+      .replace(/\/+$/, "")
+      .split("/")
+      .pop()
+      .toLowerCase();
+    if (RING_PROGRAMS.has(pathSegment)) {
+      return pathSegment;
+    }
+
+    const queryProgram = new URLSearchParams(window.location.search)
+      .get("ring")
+      ?.toLowerCase();
+    if (queryProgram && RING_PROGRAMS.has(queryProgram)) {
+      return queryProgram;
+    }
+
+    const hashSegment = window.location.hash
+      .replace(/^#/, "")
+      .split("?")[0]
+      .toLowerCase();
+    if (RING_PROGRAMS.has(hashSegment)) {
+      return hashSegment;
+    }
+
+    return "cs";
+};
+
 // search for website urls within tolerance of protocol, subdomain and trailing slashes
 const fuzzyMatch = (searchTerm, target) => {
     const searchTermFormatted = formatUrl(searchTerm);
@@ -13,4 +43,4 @@ const fuzzyMatch = (searchTerm, target) => {
     return searchTermFormatted.includes(targetFormatted) || targetFormatted.includes(searchTermFormatted);
 };
 
-export { fuzzyMatch, formatUrl };
+export { fuzzyMatch, formatUrl, getProgramFromUrl };
